9ec09bcfb0d5160221d3656dd3504e0e3d335796
fboulnois <fboulnois@users.noreply.github.com>
2024-12-26 15:12:16 -0500

    feat: upgrade to svelte 5

--- eslint.config.js
+++ eslint.config.js
@@ -3,9 +3,12 @@ import ts from 'typescript-eslint';
 import svelte from 'eslint-plugin-svelte';
 import prettier from 'eslint-config-prettier';
 import globals from 'globals';
+import { includeIgnoreFile } from '@eslint/compat';
+import { fileURLToPath } from 'node:url';
+const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
 
-/** @type {import('eslint').Linter.FlatConfig[]} */
-export default [
+export default ts.config(
+	includeIgnoreFile(gitignorePath),
 	js.configs.recommended,
 	...ts.configs.recommended,
 	...svelte.configs['flat/recommended'],
@@ -35,4 +38,4 @@ export default [
 			'@typescript-eslint/no-unused-vars': [1, { argsIgnorePattern: '^_' }]
 		}
 	}
-];
+);
